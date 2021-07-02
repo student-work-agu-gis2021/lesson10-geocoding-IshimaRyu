ca87afcb5e1b5eaa7a0a183e1f4702c663103ac6
IshimaRyu <82187608+IshimaRyu@users.noreply.github.com>
2021-07-02 15:30:36 +0000

    I calculate the sum of population living within 1.5 km for each shopping center.

--- Exercise_10_Problem_1_3.py
+++ Exercise_10_Problem_1_3.py
@@ -137,6 +137,9 @@ azabu = join.loc[join["name"] == "Seibu Shibuya Store"]
 seibu = join.loc[join["name"] == 'National Azabu']
 
 # YOUR CODE HERE 11 to report how many people live within 1.5 km distance from each shopping center
+tokyu_sum=round(tokyu["PTN_2020"].sum())
+seibu_sum=round(seibu["PTN_2020"].sum())
+azabu_sum=round(azabu["PTN_2020"].sum())
 
 
 # **Reflections:**
